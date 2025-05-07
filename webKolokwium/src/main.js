ea0f5deb45d1bf4ba9ab3718819b653e5e1ab11b
Piotr Modliszewski <piotr.modliszewski01@gmail.com>
2025-05-07 14:12:10 +0200

    dodanie plików scss i usunięcie css

--- webKolokwium/src/main.js
+++ webKolokwium/src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import './scss/style.scss'
 
 document.querySelector('#app').innerHTML = `
   <div>
